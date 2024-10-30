08a4f971caedc21abf6d52e591e02e3d7c6ac700
David Mansilla <rdavidmansilla@gmail.com>
2024-10-30 07:44:57 -0600

    feature: base project structure and wordFinder class

---
+++ CelaraLabsChallenge/Program.cs
@@ -0,0 +1,33 @@
+﻿using CelaraLabsChallenge;
+
+var matrix = new List<string>
+        {
+            "coldwind",
+            "chillbrrr",
+            "warmheat",
+            "frozensn",
+            "sleetice",
+            "snowfall",
+            "breezezz",
+            "rainhail"
+        };
+
+var wordStream = new List<string>
+        {
+            "cold", "wind", "chill", "heat", "snow", "breeze", "rain", "sun", "ice", "hail", "storm", "snow"
+        };
+
+var wordFinder = new WordFinder(matrix);
+
+var foundWords = wordFinder.Find(wordStream);
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Top found words: \n");
+Console.ForegroundColor = ConsoleColor.Green;
+
+foreach (var word in foundWords)
+{
+    Console.WriteLine(word);
+}
+
+Console.ForegroundColor = ConsoleColor.White;
---
+++ CelaraLabsChallenge/WordFinder.cs
@@ -0,0 +1,87 @@
+﻿namespace CelaraLabsChallenge
+{
+    /// <summary>
+    /// Base finder class to look for the words in 2 dimensions, horizontal and vertical in the matrix.
+    /// </summary>
+    public class WordFinder
+    {
+        private readonly char[][] _matrix;
+        private readonly int _rows;
+        private readonly int _cols;
+
+        public WordFinder(IEnumerable<string> matrix)
+        {
+            _matrix = matrix.Select(row => row.ToCharArray()).ToArray();
+            _rows = _matrix.Length;
+            _cols = _matrix[0].Length;
+        }
+
+        public IEnumerable<string> Find(IEnumerable<string> wordstream)
+        {
+            var wordSet = new HashSet<string>(wordstream); // Unique words for search
+            var foundWords = new Dictionary<string, int>(); // Track word occurrences
+
+            foreach (var word in wordSet)
+            {
+                if (ExistsInMatrix(word))
+                {
+                    foundWords[word] = foundWords.TryGetValue(word, out int value) ? value + 1 : 1;
+                }
+            }
+
+            IEnumerable<string> wordsFiltered =
+                foundWords.OrderByDescending(w => w.Value).Take(10).Select(w => w.Key); // Taking top 10 most repeated words.
+
+            return wordsFiltered;
+        }
+
+        private bool ExistsInMatrix(string word)
+        {
+            int wordLen = word.Length;
+
+            for (int r = 0; r < _rows; r++)
+            {
+                for (int c = 0; c < _cols; c++)
+                {
+                    if (SearchHorizontally(word, wordLen, r, c) || SearchVertically(word, wordLen, r, c))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool SearchHorizontally(string word, int wordLen, int row, int col)
+        {
+            if (col + wordLen <= _cols)
+            {
+                for (int i = 0; i < wordLen; i++)
+                {
+                    if (_matrix[row][col + i] != word[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+
+        private bool SearchVertically(string word, int wordLen, int row, int col)
+        {
+            if (row + wordLen <= _rows)
+            {
+                for (int i = 0; i < wordLen; i++)
+                {
+                    if (_matrix[row + i][col] != word[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
